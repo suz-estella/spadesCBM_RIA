e44457b5fe5ec05f957fbbc154614cd0e735498a
Susan Murray <murray.e.susan@gmail.com>
2025-03-08 10:04:34 -0800

    test set up update

--- tests/testthat/devel-runTests.R
+++ tests/testthat/devel-runTests.R
@@ -20,12 +20,16 @@
 
   # Suppress warnings from calls to setupProject, simInit, and spades
   options("spades.test.suppressWarnings" = TRUE)
-
-  # Set custom input data location
-  options("reproducible.inputPaths" = NULL)
+  
+  # Set custom directory paths
+  ## Speed up tests by allowing inputs, cache, and R packages to persist between runs
+  options("spades.test.paths.inputs"   = NULL) # inputPath
+  options("spades.test.paths.cache"    = NULL) # cachePath
+  options("spades.test.paths.packages" = NULL) # packagePath
 
   # Test recreating the Python virtual environment
-  options("spades.test.virtualEnv" = TRUE)
+  ## WARNING: this will slow down testing, avoid unless Python is having issues
+  Sys.setenv(RETICULATE_VIRTUALENV_ROOT = file.path(tempdir(), "virtualenvs"))
 
 
 ## RUN ALL TESTS ----
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -21,15 +21,3 @@ spadesTestPaths <- SpaDEStestSetUpDirectories(
   moduleRepos = getOption("spades.test.modules"),
   require     = "googledrive"
 )
-
-## Store cache in RProject
-if (!testthat::is_testing()) spadesTestPaths$temp$cache <- file.path(spadesTestPaths$RProj, "tests/cache")
-
-# Recreate the Python virtual environment location
-if (getOption("spades.test.virtualEnv", default = FALSE)){
-  dir.create(file.path(spadesTestPaths$temp$root, "virtualenvs"))
-  withr::local_envvar(
-    list(RETICULATE_VIRTUALENV_ROOT = file.path(spadesTestPaths$temp$root, "virtualenvs")),
-    .local_envir = if (testthat::is_testing()) testthat::teardown_env() else parent.frame())
-}
-
